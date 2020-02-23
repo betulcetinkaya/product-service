c98865ff83f45c3622992fbaf7f2fcfdc8a950c0
betulc <betul.cetinkaya@sistek.com.tr>
2020-02-23 22:35:37 +0300

    exception class fix

--- src/main/java/com/assignment/product/exception/ProductNotFoundException.java
+++ src/main/java/com/assignment/product/exception/ProductNotFoundException.java
@@ -1,28 +1,10 @@
 package com.assignment.product.exception;
 
-import org.springframework.data.domain.PageImpl;
 
 public final class ProductNotFoundException extends RuntimeException {
-
-    private String id;
-
-    public ProductNotFoundException() {
-    }
-
-    public ProductNotFoundException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
 
     public ProductNotFoundException(final String message) {
         super(message);
     }
 
-    public ProductNotFoundException(String id, final String message) {
-        super(message);
-        this.id = id;
-    }
-
-    public ProductNotFoundException(final Throwable cause) {
-        super(cause);
-    }
 }
